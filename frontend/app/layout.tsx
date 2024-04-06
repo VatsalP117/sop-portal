96ce3de8ee3b91e07585e1ec41dc7c57a65fa979
Vatsal Patel <f20212460@goa.bits-pilani.ac.in>
2024-04-07 01:39:49 +0530

    sonner bt sorted

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Footer from "@/components/footer";
+import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,6 +26,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
           <Footer />
         </ThemeProvider>
       </body>
