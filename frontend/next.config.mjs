f4db1e472268993778fef4292150dec77675eb7a
Vatsal Patel <f20212460@goa.bits-pilani.ac.in>
2024-04-07 09:16:05 +0530

    final

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://sopportal.azurewebsites.net/api/:path*",
+        destination: "http://localhost:5000/api/:path*",
       },
     ];
   },
