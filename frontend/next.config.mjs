5848aadbfba9191dac8fde26c7ff3bf2153c3fe6
Vatsal Patel <f20212460@goa.bits-pilani.ac.in>
2024-08-29 16:29:28 +0530

    bug fixes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return process.env.NODE_ENV === "development"
-      ? [
-          {
-            source: "/api/:path*",
-            destination: "http://localhost:5000/api/:path*",
-          },
-        ]
-      : [];
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/api/:path*",
+      },
+    ];
   },
   images: {
     domains: ["github.com"],
